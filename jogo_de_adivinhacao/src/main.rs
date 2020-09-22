aa6ee9fa3b727735fdb112a1cee78e9ed39c386f
Renan Zapelini <renanzapelini@hotmail.com>
2020-09-22 13:21:44 -0300

    Joguinho de adivinhação conforme a documentação do Rust

---
+++ jogo_de_adivinhacao/src/main.rs
@@ -0,0 +1,39 @@
+extern crate rand;
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    let numero_secreto = rand::thread_rng().gen_range(1, 101);
+
+    println!("Adivinhe o número!");
+
+    loop {
+        println!("Digite seu palpite: ");
+
+        let mut palpite = String::new();
+
+        io::stdin()
+            .read_line(&mut palpite)
+            .expect("Falha ao ler entrada");
+
+        let palpite: u32 = match palpite
+            .trim()
+            .parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+
+        println!("Você disse: {}", palpite);
+
+        match palpite.cmp(&numero_secreto) {
+            Ordering::Less => println!("Muito baixo!"),
+            Ordering::Greater => println!("Muito alto!"),
+            Ordering::Equal => {
+                println!("Você acertou!");
+                break;
+            },
+        }
+    }
+}
